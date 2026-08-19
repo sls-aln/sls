f5c7dd3964372a627b3cf5dabefedc267e36d89f
sls-aln <alina.v.georgievna@mail.ru>
2026-08-19 23:46:00 +0900

    Переключился на SQLite для деалоя

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2smmhdct.@localhost:5432/notes_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
